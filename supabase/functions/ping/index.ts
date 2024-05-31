03066a806aed68ee8ab7ce7b4d2d2f13dee3204c
ayushdungriyal2 <ayushdungriyal2@gmail.com>
2024-06-01 02:20:50 +0530

    fixed yml

--- supabase/functions/ping/index.ts
+++ supabase/functions/ping/index.ts
@@ -1,6 +1,6 @@
 Deno.serve(() => {
   const data = {
-    message: 'GitHub Actions + Deno Deploy Success 🚀',
+    message: 'GitHub Actions + Deno Deploy Success!',
   };
 
   return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
