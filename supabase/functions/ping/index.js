25beb81a0e70588342d3dff249646170fbb44225
ayushdungriyal2 <ayushdungriyal2@gmail.com>
2024-06-01 04:32:03 +0530

    changed to ts

--- supabase/functions/ping/index.js
+++
@@ -1,7 +0,0 @@
-Deno.serve(() => {
-  const data = {
-    message: 'GitHub Actions + Deno Deploy Success!',
-  };
-
-  return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
-});
