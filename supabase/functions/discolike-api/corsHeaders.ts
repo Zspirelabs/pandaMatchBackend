0c85a73cc61e76be7788395c96dc2a1babcc12dc
Griffin Annshual <griffintbr@gmail.com>
2024-06-09 00:08:47 +0530

    Fixed the backend api discolike-api

--- supabase/functions/discolike-api/corsHeaders.ts
+++ supabase/functions/discolike-api/corsHeaders.ts
@@ -1,6 +1,5 @@
-
 export const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers":
-      "authorization, X-Client-Id, apikey, content-type",
-  };
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers":
+		"authorization, x-client-info, apikey, content-type",
+}
--- supabase/functions/discolike-api/index.ts
+++ supabase/functions/discolike-api/index.ts
@@ -1,73 +1,102 @@
-import axiod from "https://deno.land/x/axiod/mod.ts";
-import { deductCredits } from "./deductCredits.ts";
-import { corsHeaders } from "./corsHeaders.ts";
+import axiod from "https://deno.land/x/axiod/mod.ts"
+import { deductCredits } from "./deductCredits.ts"
+import { corsHeaders } from "./corsHeaders.ts"
+
+const paramsSerializer = (params) => {
+	const searchParams = new URLSearchParams()
+
+	for (const key in params) {
+		if (Array.isArray(params[key])) {
+			params[key].forEach((value) => {
+				searchParams.append(key, value)
+			})
+		} else {
+			searchParams.append(key, params[key])
+		}
+	}
+
+	return searchParams.toString()
+}
 
 // Initialize a Deno server to handle API requests
 Deno.serve(async (req) => {
+	if (req.method === "OPTIONS") {
+		return new Response(null, {
+			status: 204,
+			headers: corsHeaders,
+		})
+	}
 
-  // Only allow GET requests; reject others with a 405 Method Not Allowed
+	if (req.method !== "GET" && req.method !== "POST") {
+		return new Response("Method Not Allowed", {
+			status: 405,
+			headers: { ...corsHeaders, "Content-Type": "application/json" },
+		})
+	}
 
-  if (req.method === "OPTIONS") {
-    return new Response(null, {
-      status: 204,
-      headers: corsHeaders,
-    });
-  }
-  else if (req.method !== "GET") {
-    return new Response("Method Not Allowed", {
-      status: 405,
-      headers: { ...corsHeaders, "Content-Type": "application/json" },
-    });
-  }
+	try {
+		const body = await req.json()
+		const { params, user_id } = body
 
-  try {
-    // Extract query parameters and client ID from request headers
-    const url = new URL(req.url);
-    const requestQuery = url.searchParams;
-    const user_id = req.headers.get("X-Client-Id");
+		const response = await axiod.get("https://api.discolike.com/v1/discover", {
+			params: params,
+			paramsSerializer: paramsSerializer,
+			withCredentials: true,
+			headers: {
+				"x-discolike-key": "5130dbdc-9bbb-4254-94d8-25d8b4a8ee1e",
+				"X-Client-Id": user_id
+			},
+		})
 
-    // API call to Discolike service
-    const response = await axiod.get("https://api.discolike.com/v1/discover", {
-      headers: {
-        "x-discolike-key": "5130dbdc-9bbb-4254-94d8-25d8b4a8ee1e",
-        "X-Clinet-Id": user_id,
-      },
-      params: Object.fromEntries(requestQuery),
-    });
+		const data = response.data
 
-    const data = response.data;
+		if (response.status === 200) {
+			const creditAmount = params.nl_match ? 2 : 1
+			console.log("Credit Amount:", creditAmount)
+			try {
+				console.log("Deducting Credits:", creditAmount)
+				await deductCredits(user_id, creditAmount)
+				console.log("Credits Deducted Successfully:", creditAmount)
+			} catch (deductError) {
+					return new Response(
+						JSON.stringify({
+							response: deductError.message,
+						}),
+						{
+							status: 400,
+							headers: { ...corsHeaders, "Content-Type": "application/json" },
+						}
+					)
+			}
+		} else {
+			console.log("No credits deducted for non-success response.")
+		}
 
-    // Conditional credit updating based on response status and 'nl_match' flag
-    if (response.status === 200) {
-      // Using the ternary operator to check explicitly for null or undefined
-      const creditAmount = (requestQuery.get("nl_match") != null) ? 2 : 1;
-      console.log("Credit Amount:", creditAmount);
-      try {
-        console.log("Deducting Credits:", creditAmount);
-        await deductCredits(user_id, creditAmount);
-        console.log("Credits Deducted Successfully", creditAmount);
-      } catch (deductError) {
-        console.error("Deduct Credits Error:", deductError.message);
-      }
-    }
-    else {
-      console.log("No credits deducted for non-success response.");
-    }
-    
-
-    // Return API response in JSON format
-    return new Response(JSON.stringify(data), {
-      headers: { ...corsHeaders, "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    console.error("Error:", error.message);
-    // Return an error response in case of failure
-    return new Response(JSON.stringify({
-      error: "An error occurred while processing your request.",
-      details: error.message,
-    }), {
-      status: 400,
-      headers: { ...corsHeaders, "Content-Type": "application/json" },
-    });
-  }
-});
+		return new Response(JSON.stringify(data), {
+			headers: { ...corsHeaders, "Content-Type": "application/json" },
+		})
+	} catch (error) {
+		console.error("Error making the API call:", error)
+		if (error.response.data.detail) {
+			return new Response(
+				JSON.stringify({
+					response: error.response.data.detail,
+				}),
+				{
+					status: 400,
+					headers: { ...corsHeaders, "Content-Type": "application/json" },
+				}
+			)
+		} else {
+			return new Response(
+				JSON.stringify({
+					response: "Invalid request",
+				}),
+				{
+					status: 400,
+					headers: { ...corsHeaders, "Content-Type": "application/json" },
+				}
+			)
+		}
+	}
+})
